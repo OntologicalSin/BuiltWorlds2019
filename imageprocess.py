2c14fcbf0c335f7825bc3153f0f51055d889cd20
OntologicalSin <nicklyubenko@gmail.com>
2019-10-05 03:06:04 -0500

    starting day 2

--- imageprocess.py
+++ imageprocess.py
@@ -3,8 +3,6 @@ from functools import reduce
 from PIL import Image
 
 
-
-
 def phash(img):
 
     img = img.resize((8, 8), Image.ANTIALIAS).convert('L')
@@ -14,7 +12,6 @@ def phash(img):
     return hash_value
 
 def hamming_distance(a, b):
-
     hm_distance=bin(a ^ b).count('1')
     print(hm_distance)
     return hm_distance
@@ -22,17 +19,13 @@ def hamming_distance(a, b):
 
 
 def is_imgs_similar(img1,img2):
-
-
     return True if hamming_distance(phash(img1),phash(img2)) <= 5 else False
 
-
-
 if __name__ == '__main__':
-
-
-    sensitive_pic = Image.open("support/2019-07-17-16-13-09-511.jpg")
-    target_pic = Image.open("support/2019-07-17-16-15-29-586.jpg")
+    exp1 = Image.open("example1.jpg")
+    exp2 = Image.open("example1.jpg")
+    exp3 = Image.open("example1.jpg")
+    exp3 = Image.open("example1.jpg")
 
 
     result=is_imgs_similar(target_pic, sensitive_pic)
